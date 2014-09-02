91785e63661e1066b8efb44782e4daa07758cc05
Jari Torniainen <jtorniai@kalervo>
2014-09-02 14:58:39 +0300

    more examples

---
+++ examples/recorder_example.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from lsltools import sim,rec
+import time
+
+# STEP 1: Initialize and start up three data streams
+data1 = sim.EEGData(nch=3)
+data2 = sim.RandomData(srate=128)
+data3 = sim.LinearData(srate=64)
+data1.start()
+data2.start()
+data3.start()
+
+# STEP 2: Initialize recorder to save three streams of step 1 to a file
+streams = rec.pylsl.resolve_streams()
+recorder = rec.StreamRecorder('/tmp/example_recording.xdf',streams)
+
+# STEP 3: Start recorder and let it run for 30s 
+recorder.start_recording()
+time.sleep(10)
+recorder.end_recording()
+
+
+data1.stop()
+data2.stop()
+data3.stop()
--- examples/visualizer_example.py
+++ examples/visualizer_example.py
@@ -1,12 +1,14 @@
-#shebangs here
+#!/usr/bin/env python3
 
 from lsltools import sim,vis
 
+
+# STEP 1: Initialize a generator for simulated EEG and start it up.
 eeg_data = sim.EEGData(nch=3,stream_name="example")
 eeg_data.start()
 
-
+# STEP 2: Find the stream started in step 1 and pass it to the vis.Grapher
 streams = vis.pylsl.resolve_byprop("name","example")
 eeg_graph = vis.Grapher(streams[0],512*5,'y')
 
-
+# STEP 3: Enjoy the graph.
